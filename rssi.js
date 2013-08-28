f11ea5ebe0889f75d20868e6b812b6857480a971
Tako Neko <oct8cat@gmail.com>
2013-08-28 08:46:42 +0400

    AMD support.

--- rssi.js
+++ rssi.js
@@ -1,13 +1,30 @@
 (function () {
+
+  if(typeof define === 'undefined') {
+    /**
+     * Simulates asynchronous module definition.
+     * @param {mixed} d Module definition. Can be a function or an object.
+     */
+    function define(d) {
+      if (typeof module !== 'undefined' && module.exports)
+        module.exports = (typeof d === 'function') ? d() : d
+    }
+  }
+
+  define(function() {
+    /** @module rssi */
+
     function rewrite(foo, bar) {
-        return '"+(typeof obj["' + bar + '"]!="undefined"?obj["' + bar + '"]:"' + foo + '")+"'
+      return '"+(typeof obj["' + bar + '"]!="undefined"?obj["' + bar + '"]:"' + foo + '")+"'
     }
 
-    function fmt(input) {
-        var out = JSON.stringify(input).replace(/#\{(.*?)\}/g, rewrite)
-        /* jshint evil: true */
-        return Function('obj', 'return ' + out)
+    var exports = function(input) {
+      var out = JSON.stringify(input).replace(/#\{(.*?)\}/g, rewrite)
+      /* jshint evil: true */
+      return Function('obj', 'return ' + out)
     }
 
-    if (typeof module != 'undefined' && module.exports) module.exports = fmt
+    return exports
+  })
+
 }())
