28c0c163ae0da79ce80512dd39b4a3586b261507
Viktor Konovalov <kde3.5virpool@gmail.com>
2013-09-04 00:38:00 +0300

    Add ability to disable template caching

--- rssi.js
+++ rssi.js
@@ -4,16 +4,35 @@
 (function () {
     var cache = {}
 
+    var defaults = {
+        noCache: false
+    }
+
+    function copy(src, dst) {
+        for (var key in src) if (Object.prototype.hasOwnProperty.call(src, key))
+            dst[key] = src[key]
+    }
+
+    function extend(target, src, otherOne) {
+        copy(src, target)
+        if (otherOne) copy(otherOne, target)
+        return target
+    }
+
     function rewrite(foo, bar) {
         return '"+(typeof obj["' + bar + '"]!="undefined"?obj["' + bar + '"]:"' + foo + '")+"'
     }
 
-    function fmt(input) {
-        if (input in cache) return cache[input]
+    function fmt(input, options) {
+        options = extend({}, defaults, options)
+
+        if (options.noCache === false && input in cache) return cache[input]
 
         var out = JSON.stringify(input).replace(/#\{(.*?)\}/g, rewrite)
-        /* jshint boss: true, evil: true */
-        return (cache[input] = Function('obj', 'return ' + out))
+        /* jshint evil: true */
+        var template = Function('obj', 'return ' + out)
+        /* jshint boss: true */
+        return options.noCache ? template : (cache[input] = template)
     }
 
     if (typeof module != 'undefined' && module.exports) module.exports = fmt
